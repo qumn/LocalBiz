ad4462246cfe10ae430db64aac9aac1917081060
qumn <2476573497@qq.com>
2023-03-10 14:05:08 +0800

    修复spring升级不兼容问题

--- ruoyi-modules/ruoyi-file/src/main/java/com/ruoyi/file/RuoYiFileApplication.java
+++ ruoyi-modules/ruoyi-file/src/main/java/com/ruoyi/file/RuoYiFileApplication.java
@@ -3,15 +3,16 @@ package com.ruoyi.file;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import com.ruoyi.common.swagger.annotation.EnableCustomSwagger2;
+import org.springframework.context.annotation.Import;
 
 /**
  * 文件服务
  * 
  * @author ruoyi
  */
-@EnableCustomSwagger2
+//@EnableCustomSwagger2
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@Import({com.github.tobato.fastdfs.FdfsClientConfig.class})
 public class RuoYiFileApplication
 {
     public static void main(String[] args)
